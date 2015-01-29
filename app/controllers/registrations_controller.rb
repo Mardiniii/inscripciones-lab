1e7443e1699c39ec8346aab2769fc91c4c423d51
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-01-29 15:48:17 -0500

    Add method welcome and features tu star with the registration process

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,8 @@
 class RegistrationsController < ApplicationController
+	def welcome_registration
+		club_id = params[:club_id]
+		tournament_id = params[:tournament_id]
+		@club = Club.find(club_id)
+		@tournament = Tournament.find(tournament_id)
+	end
 end
