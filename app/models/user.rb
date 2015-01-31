f60fa4374927ee2dec6a2f67c73fcca85fb3f5c6
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-01-31 12:12:18 -0500

    Add column role to User, define enum into User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [ :user, :admin]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :club, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131165037) do
+ActiveRecord::Schema.define(version: 20150131170739) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 20150131165037) do
     t.string   "identification_number"
     t.string   "last_name"
     t.string   "cellphone"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
