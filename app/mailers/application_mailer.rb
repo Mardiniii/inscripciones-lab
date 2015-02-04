8a48c04a4655666d136322d977d08f0c30296448
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-02-04 12:10:06 -0500

    Add mailer to send notification to user after, create Club

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Liga Antioqueña de Baloncesto"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     @url  = 'http://eventoslab.com'
     mail(to: @user.email, subject: 'Bienvenido al sistema de inscripciones de la Liga Antioqueña de Baloncesto')
 	end
+
+	def welcome_club_email(club)
+		@user = club.user
+		@club = club
+		mail(to: @user.email; sezama08@gmail.com, subject: 'Bienvenido a Inscripciones - LAB, haz registrado un nuevo Club')
+	end
 end
