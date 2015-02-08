275e52ebc583bb1c2c8f1afc8767c40e5f7603fd
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-02-08 15:05:00 -0500

    Change Field Types, form for new Field and show Field view

--- app/models/field.rb
+++ app/models/field.rb
@@ -11,9 +11,9 @@
 #
 
 class Field < ActiveRecord::Base
-	enum field_type: [:Coliseo_Madera,:Coliseo_Baldosa,:Coliseo_Concreto,:Coliseo_Asfalto,:Coliseo_Sintética,:Placa_Baldosa,:Placa_Concreto,:Placa_Asfalto,:Placa_Sintética]
+	enum field_type: [:coliseo_madera,:coliseo_baldosa,:coliseo_concreto,:coliseo_asfalto,:coliseo_sintética,:placa_baldosa,:placa_concreto,:placa_asfalto,:placa_sintética]
   belongs_to :club
   has_many :field_hours, dependent: :destroy
-  accepts_nested_attributes_for :field_hours, :reject_if => :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :field_hours, reject_if: proc { |attributes| attributes['day_id'].blank? }, allow_destroy: true
   validates_associated :field_hours
 end
