7d934f927ecb595e5eff8086839cfd790ad329f7
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-02-09 11:16:07 -0500

    Add jQuery validation for Register fields into Inscription Nested Form

--- app/assets/javascripts/inscriptions.js
+++ app/assets/javascripts/inscriptions.js
@@ -1,17 +1,17 @@
 $(document).on("page:change", function() {
 	$(".new_register").on('change','select.type-register',function(){
-		var option = $(this).val();
-		console.log("Selecciono la opcion: "+option);
-		var coach = ('select.type-register option:selected[value="1"]').length;
-		var players = $('select.type-register option:selected[value="2"]').length;
-		console.log(""+players+" Jugadores inscritos");
-		if (coach > 1 && option == 1){
-		  alert("Solo puede registrar un entrenador por inscripción");
-		  $(this).val('');
-		}
-		if (players > 20 && option == 2){
-		  alert("RECUERDE: Solo puede registrar 20 Jugadores por inscripción");
-		  $(this).val('');
-		}
+		// var option = $(this).val();
+		alert("Click in select");
+		// var coach = ('select.type-register option:selected[value="1"]').length;
+		// var players = $('select.type-register option:selected[value="2"]').length;
+		// console.log(""+players+" Jugadores inscritos");
+		// if (coach == 1 && option == 1){
+		//   alert("Solo puede registrar un entrenador por inscripción");
+		//   $(this).val('');
+		// }
+		// if (players > 20 && option == 2){
+		//   alert("RECUERDE: Solo puede registrar 20 Jugadores por inscripción");
+		//   $(this).find('option:selected').prop("selected", false);
+		// }
 	});
 });
