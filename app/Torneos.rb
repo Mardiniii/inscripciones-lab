1263cf67743f53a781ac59d5f377a77ccc95a63c
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-02-10 12:31:51 -0500

    Add tournaments for initial version of the platform

--- app/Torneos.rb
+++ app/Torneos.rb
@@ -1,36 +1,36 @@
-# FESTIVALES MINIBALONCESTO
+# FESTIVALES MINIBALONCESTO - AGREGADO
 
 Tournament.create(categorie_id: 1, name: "Primer Festival de Minibaloncesto Masculino - 2015", start_date: Date.new(2015,02,28), end_date: Date.new(2015,02,28), description: "Evento deportivo para niños de 10 a 12 años de edad, en el cual juegan 3 partidos por día - Valor Inscripción: $60.000 por equipo (Valor Anual: $400.000) - Ubicación: Liceo Salazar Y Herrera, Calle 42C #86 - 17", deadline: Date.new(2015,02,25))
 Tournament.create(categorie_id: 1, name: "Primer Festival de Minibaloncesto Femenino - 2015", start_date: Date.new(2015,02,28), end_date: Date.new(2015,02,28), description: "Evento deportivo para niños de 10 a 12 años de edad, en el cual juegan 3 partidos por día - Valor Inscripción: $60.000 por equipo (Valor Anual: $400.000) - Ubicación: Liceo Salazar Y Herrera, Calle 42C #86 - 17", deadline: Date.new(2015,02,25))
 
-# TORNEO DEPARTAMENTAL INFANTIL
+# TORNEO DEPARTAMENTAL INFANTIL - AGREGADO
 
 Tournament.create(categorie_id: 3, name: "Torneo Departamental Infantil Masculino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 2001 y 2002 - Valor inscripción: $550.000", deadline: Date.new(2015,02,23))
 Tournament.create(categorie_id: 3, name: "Torneo Departamental Infantil Femenino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 2001 y 2002 - Valor inscripción: $550.000", deadline: Date.new(2015,02,23))
 
-# TORNEO DEPARTAMENTAL JUNIOR
+# TORNEO DEPARTAMENTAL JUNIOR - AGREGADO
 
 Tournament.create(categorie_id: 4, name: "Torneo Departamental Junior Masculino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1999 y 2000 - Valor inscripción: $600.000", deadline: Date.new(2015,02,23))
 Tournament.create(categorie_id: 4, name: "Torneo Departamental Junior Femenino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1999 y 2000 - Valor inscripción: $600.000", deadline: Date.new(2015,02,23))
 
-# TORNEO DEPARTAMENTAL JUVENIL
+# TORNEO DEPARTAMENTAL JUVENIL - AGREGADO
 
 Tournament.create(categorie_id: 5, name: "Torneo Departamental Juvenil Masculino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1997 y 1998 - Valor inscripción: $650.000", deadline: Date.new(2015,02,23))
 Tournament.create(categorie_id: 5, name: "Torneo Departamental Juvenil Femenino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,10,31), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1997 y 1998 - Valor inscripción: $650.000", deadline: Date.new(2015,02,23))
 
-# TORNEO DEPARTAMENTAL ASCENSO
+# TORNEO DEPARTAMENTAL ASCENSO - AGREGADO
 
 Tournament.create(categorie_id: 6, name: "Torneo Departamental Ascenso Sub-21 Masculino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,8,30), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1994, 1995 y 1996 - Valor inscripción: $750.000", deadline: Date.new(2015,02,23))
 Tournament.create(categorie_id: 6, name: "Torneo Departamental Ascenso Sub-21 Femenino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,8,30), description: "Evento de la Liga Antioqueña de Baloncesto para los deportistas nacidos en el año 1994, 1995 y 1996 - Valor inscripción: $750.000", deadline: Date.new(2015,02,23))
 
-# TORNEO DE PRIMERA DEPARTAMENTAL
+# TORNEO DE PRIMERA DEPARTAMENTAL - AGREGADO
 
 Tournament.create(categorie_id: 7, name: "Torneo de Primera Departamental Masculino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,8,30), description: "Evento de mayores con categoría libre - Valor inscripción: $850.000", deadline: Date.new(2015,02,23))
 Tournament.create(categorie_id: 7, name: "Torneo de Primera Departamental Femenino - 2015", start_date: Date.new(2015,02,27), end_date: Date.new(2015,8,30), description: "Evento de mayores con categoría libre - Valor inscripción: $850.000", deadline: Date.new(2015,02,23))
 
-# TORNEO DEPARTAMENTAL RECREATIVO - Falta Categoria para estos
+# TORNEO DEPARTAMENTAL RECREATIVO - Falta Categoria para estos - AGREGADO
 
-Tournament.create(categorie_id: , name: "Torneo Departamental Recreativo Masculino", start_date: Date.new(2015,02,27), end_date: Date.new(2015,06,13), description: "Evento para equipos en el ambito recreativo, dirigido a deportistas nacidos a partir del año 1985 (30 años) - Valor inscripción: $700.000 por semestre", deadline: Date.new(2015,02,23))
-Tournament.create(categorie_id: , name: "Torneo Departamental Recreativo Masculino", start_date: Date.new(2015,02,27), end_date: Date.new(2015,06,13), description: "Evento para equipos en el ambito recreativo, dirigido a deportistas nacidos a partir del año 1985 (30 años) - Valor inscripción: $700.000 por semestre", deadline: Date.new(2015,02,23))
+Tournament.create(categorie_id: 15, name: "Torneo Departamental Recreativo Masculino - Primer Semestre", start_date: Date.new(2015,02,27), end_date: Date.new(2015,06,13), description: "Evento para equipos en el ambito recreativo, dirigido a deportistas nacidos a partir del año 1985 (30 años) - Valor inscripción: $700.000 por semestre", deadline: Date.new(2015,02,23))
+Tournament.create(categorie_id: 15, name: "Torneo Departamental Recreativo Femenino - Primer Semestre", start_date: Date.new(2015,02,27), end_date: Date.new(2015,06,13), description: "Evento para equipos en el ambito recreativo, dirigido a deportistas nacidos a partir del año 1985 (30 años) - Valor inscripción: $700.000 por semestre", deadline: Date.new(2015,02,23))
 
 
