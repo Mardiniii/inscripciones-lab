e9efa9c3163a2b1a424404ee1a631f919b23281b
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-02-10 10:05:23 -0500

    Remove alert in new Inscription

--- app/assets/javascripts/inscriptions.js
+++ app/assets/javascripts/inscriptions.js
@@ -1,10 +1,10 @@
 $(document).on("page:change", function() {
 	$(".new_register").on('change','select.type-register',function(){
 		// var option = $(this).val();
-		alert("Click in select");
+		// alert("Click in select");
 		// var coach = ('select.type-register option:selected[value="1"]').length;
 		// var players = $('select.type-register option:selected[value="2"]').length;
-		// console.log(""+players+" Jugadores inscritos");
+		console.log("Tengo el click en el select");
 		// if (coach == 1 && option == 1){
 		//   alert("Solo puede registrar un entrenador por inscripción");
 		//   $(this).val('');
